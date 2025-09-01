d0c27685172baf0e08a956dda27c6720b7ceead9
Iuri Mignoni <iurimig@gmail.com>
2025-09-01 11:14:51 -0300

    .

--- src/Modules/ParishManagement/Modules.ParishManagement.Application/Members/Authenticate/AuthenticateMemberCommandHandler.cs
+++ src/Modules/ParishManagement/Modules.ParishManagement.Application/Members/Authenticate/AuthenticateMemberCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using Ardalis.Result;
 using BuildingBlocks.Application;
+using Microsoft.Extensions.Logging;
 using Modules.IdentityProvider.Endpoints.PublicAPI;
 using Modules.IdentityProvider.Endpoints.PublicAPI.Requests;
 using Modules.ParishManagement.Application.Members.Specifications;
@@ -9,11 +10,13 @@ namespace Modules.ParishManagement.Application.Members.Authenticate;
 
 internal class AuthenticateMemberCommandHandler(
     IIdentityProviderEndpoints userAccess,
-    IMemberRepository MemberRepository) 
+    IMemberRepository MemberRepository,
+    ILogger<AuthenticateMemberCommandHandler> logger)
     : ICommandHandler<AuthenticateMemberCommand, AuthenticateMemberResponse>
 {
     private readonly IIdentityProviderEndpoints _userAccess = userAccess;
     private readonly IMemberRepository _MemberRepository = MemberRepository;
+    private readonly ILogger<AuthenticateMemberCommandHandler> _logger = logger;
 
     public async Task<Result<AuthenticateMemberResponse>> Handle(AuthenticateMemberCommand request, CancellationToken cancellationToken)
     {
@@ -21,8 +24,10 @@ internal class AuthenticateMemberCommandHandler(
 
         var Member = await _MemberRepository.FirstOrDefaultAsync(spec, cancellationToken);
 
+        _logger.LogInformation("Usuário encontrado: {Member}", Member?.Email);
+
         if (Member == null)
-            return Result<AuthenticateMemberResponse>.NotFound();
+            return Result.Error("Usuário não encontrado");
 
         var result = await _userAccess.Login(new LoginRequest(request.Email, request.Password), cancellationToken);
 
